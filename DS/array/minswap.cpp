2d838e8fac6730ce38bc9d07376f9a1d1c7887f0
aashish-2096 <aashish2096@gmail.com>
2021-01-28 10:31:02 +0530

    Optimization and Update

--- DS/array/minswap.cpp
+++ DS/array/minswap.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Two Pointer - Sliding Window
 int main(){
     int n,k;
     cin>>n;
@@ -9,15 +10,23 @@ int main(){
         cin>>ar[i];
     }
     cin>>k;
-    int max_ = 0,min_ = 0;
+    int min_ = 0;
     for(int i=0;i<n;i++){
-        if(ar[i]>k)
-        max_++;
-        else if(ar[i]<=k){
-            if(max_!=0)
-            min_++;
-        }
+        if(ar[i]<=k)
+        min_++;
     }
-    cout<<min_<<endl;
+    int ct = 0;
+    for(int i=0;i<min_;i++){
+        if(ar[i]>k)
+            ct++;
+    }
+    int val = ct;
+    for(int i=0,j=min_;i<n-min_;i++,j++){
+        if(ar[i]>k)
+        ct--;
+        if(ar[j]>k)
+        ct++;
+    }
+    cout<<min(val,ct)<<endl;
 return 0;
 }
--- DS/array/odd_even.cpp
+++ DS/array/odd_even.cpp
@@ -10,13 +10,7 @@ int main(){
     }
     vector<int>vt;
     sort(ar,ar+n);
-    // for(int i=2;i<=n;i+=2)
-    // {
-    //     if(ar[i]<ar[i-1]){
-    //         swap(ar[i],ar[i-1]);
-    //     }
-    // }
-    for(int i=0;i<n/2;i++){
+    for(int i=0;i<=n/2;i++){
         vt.push_back(ar[i]);
         vt.push_back(ar[n-1-i]);
     }
@@ -25,6 +19,3 @@ int main(){
     }
     return 0;
 }
-
-// 18 18 9 3 16 18 11 1 13 5 6 7 16 7 16 17
-// 1 18 3 18 5 18 6 17 7 16 7 16 9 16 11 13
